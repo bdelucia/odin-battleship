1ca5188e4e935368b194ca8a9304969d75263e9f
Robert DeLucia Jr. <bdelucia@asu.edu>
2025-01-29 22:54:33 -0700

    gameboard

--- src/modules/gameBoard.js
+++ src/modules/gameBoard.js
@@ -1,8 +1,25 @@
+class Cell {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.hasShip = false;
+  }
+  getCoords() {
+    return [this.x, this.y];
+  }
+  placeShip() {
+    this.hasShip = true;
+  }
+}
+
 const gameBoard = function () {
   const columns = 10;
   const rows = 10;
   const gameBoard = Array.from(Array(rows), () => new Array(columns).fill(0));
   return {
-    placeShip(x, y, ship) {},
+    placeShip(x, y, ship) {
+      gameBoard[x][y].push(ship);
+    },
   };
 };
+module.exports(gameBoard);
